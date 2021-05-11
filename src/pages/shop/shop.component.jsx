bb5f0fd1c3083d6b350fe89a1be95658d67d5e9d
mailanandkumarsingh <mailanandkumarsingh@gmail.com>
2021-05-11 11:34:40 +0530

    Added the collections preview and item

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { SHOP_DATA } from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+class ShopPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    console.log("What is collections ", collections);
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
